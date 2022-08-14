98d66311ed442b17c03d59c55139d17d5b4f34ce
Oleksii Nadtochyi <a.nadtochey@gmail.com>
2022-08-14 11:17:12 +0300

    Example of recursion function was added.

--- js-worksheet.js
+++ js-worksheet.js
@@ -146,3 +146,13 @@ const backpackingMeals = { // combining content of objects
     dinner
 }
 console.log(backpackingMeals)
+
+// recursion
+const countdown = (value, fn, delay = 1000) => {
+    fn(value)
+    return value > 0
+    ? setTimeout(() => countdown(value - 1, fn, delay) , delay)
+    : value
+}
+const log = value => console.log(value)
+countdown(10, log)
